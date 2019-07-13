f04fd38a79d69b3ec733e807a8dd535a7771e7c0
frankfeng <17717444584@163.com>
2019-07-14 02:00:50 +0800

    build2

---
+++ build/precache-manifest.28630b0cc6bb7f13e188ef242ea4325a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63f6020ae47c7e56b17281b06fb94571",
+    "url": "/Ripple-button/build/index.html"
+  },
+  {
+    "revision": "10279f2e9d2acfa11c14",
+    "url": "/Ripple-button/build/static/css/main.31f2f96d.chunk.css"
+  },
+  {
+    "revision": "407ab141deba78abe319",
+    "url": "/Ripple-button/build/static/js/2.d042e1ad.chunk.js"
+  },
+  {
+    "revision": "10279f2e9d2acfa11c14",
+    "url": "/Ripple-button/build/static/js/main.a78a37d2.chunk.js"
+  },
+  {
+    "revision": "3a4cefdc3f70c41b45bb",
+    "url": "/Ripple-button/build/static/js/runtime~main.231b5cb8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Ripple-button/build/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.292ca60944fa6dd37bcafdb045d591ca.js"
+  "/Ripple-button/build/precache-manifest.28630b0cc6bb7f13e188ef242ea4325a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Ripple-button/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
