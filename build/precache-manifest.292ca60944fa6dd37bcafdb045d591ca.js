d01cad827ca0f3226efac1d6e4cf1b6434568f34
frankfeng <17717444584@163.com>
2019-07-14 01:54:35 +0800

    build

---
+++ build/precache-manifest.292ca60944fa6dd37bcafdb045d591ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f52836483cf56f65c907dbd1943618e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f030616b5d81b9150f0",
+    "url": "/static/css/main.31f2f96d.chunk.css"
+  },
+  {
+    "revision": "407ab141deba78abe319",
+    "url": "/static/js/2.d042e1ad.chunk.js"
+  },
+  {
+    "revision": "9f030616b5d81b9150f0",
+    "url": "/static/js/main.c2eddac8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
